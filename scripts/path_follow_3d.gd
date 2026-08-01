6dfd1973ef8e7f2c03a62ec58a530f53d26bd5e9
Temuchild6798 <myrandomemail1997@gmail.com>
2026-08-01 15:53:42 -0500

    added ragdoll to spline

---
+++ scripts/path_follow_3d.gd
@@ -0,0 +1,6 @@
+extends PathFollow3D
+
+@export var walk_speed = 2.0
+# Called when the node enters the scene tree for the first time.
+func _physics_process(delta):
+	progress += walk_speed * delta
